1b0f5e1bc9e100e253eb4cdeffba0f2409c0f926
M1056455 <M1056455@host.docker.internal>
2020-04-22 09:31:53 +0530

    Second Commit

---
+++ discovery-server-eureka/src/test/java/com/springbootms/discoveryservereureka/DiscoveryServerEurekaApplicationTests.java
@@ -0,0 +1,14 @@
+package com.springbootms.discoveryservereureka;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
